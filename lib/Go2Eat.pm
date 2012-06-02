0757383b13439102905415daa7b1fb09786dbc7c
xiaomo <code.vvip@gmail.com>
2012-06-02 20:03:36 +0800

    todo: fix the login post bug

--- lib/Go2Eat.pm
+++ lib/Go2Eat.pm
@@ -8,7 +8,15 @@ use Data::Dump qw/dump/;
 
 our $VERSION = '0.1';
 
-get '/'        => sub { template 'index'; };
-get '/about/?' => sub { template 'about'; };
+get  '/'        => sub { template 'index'; };
+get  '/about/?' => sub { template 'about'; };
+any  '/login/?' => sub {
+    #my ($username, $userpass) = map { params->{$_} } qw/username userpass/;
+    if( request->method eq 'POST' ){
+        return params->{username};
+    } else {
+        template 'index';
+    }
+};
 
 true;
