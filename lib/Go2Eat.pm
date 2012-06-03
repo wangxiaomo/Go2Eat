1da3123eb6477ec147a291b39bdfb9da19864936
xiaomo <code.vvip@gmail.com>
2012-06-03 19:02:27 +0800

    add login

--- lib/Go2Eat.pm
+++ lib/Go2Eat.pm
@@ -6,17 +6,46 @@ use Cwd;
 use Sys::Hostname;
 use Data::Dump qw/dump/;
 
-our $VERSION = '0.1';
+use Helper;
 
-get  '/'        => sub { template 'index'; };
+our $VERSION = '0.1';
+my $flash;
+set session => 'Simple';
+
+sub set_flash {
+    my $msg = shift;
+    $flash = $msg;
+}
+
+sub get_flash {
+    my $msg = $flash;
+    $flash  = '';
+    return $msg;
+}
+
+get  '/'        => sub { template 'index',{msg=>get_flash()}; };
 get  '/about/?' => sub { template 'about'; };
 any  '/login/?' => sub {
-    #my ($username, $userpass) = map { params->{$_} } qw/username userpass/;
     if( request->method eq 'POST' ){
-        return params->{username};
-    } else {
-        template 'index';
+        my ($username, $userpass) = map {params->{$_}} qw/username userpass/;
+        my $user = Helper::login_isvalid($username, $userpass);
+        if($user){
+            #login success
+            session is_login => 1;
+            session username => $username;
+        }else{
+            #login failed
+            set_flash('username or userpass unvalid!');
+        }
     }
+    template 'index', {
+        msg => get_flash(),
+    };
+};
+get '/logout' => sub {
+    session->destroy;
+    set_flash('logout');
+    redirect '/';
 };
 
 true;
--- lib/Helper.pm
+++ lib/Helper.pm
@@ -8,15 +8,17 @@ sub GetMongo {
     return MongoDB::Connection->new;
 }
 
-1;
+sub login_isvalid {
+    my ($username, $userpass) = @_;
+    my $conn  = GetMongo();
+    my $users = $conn->go2eat->users;
 
-package main;
-use Data::Dumper;
-
-my $conn  = Helper::GetMongo;
-my $users = $conn->go2eat->users;
-my $ret   = $users->find();
-
-while (my $user = $ret->next) {
-    print Dumper($user);
+    my $user  = $users->find_one({
+        username    =>  $username,
+        userpass    =>  $userpass,
+    });
+    return 0 if not defined $user;
+    return $user;
 }
+
+1;
